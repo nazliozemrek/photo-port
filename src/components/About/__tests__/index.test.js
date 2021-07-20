490652b4d9469e9c08812324ed4be45316a62914
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2021-07-19 23:46:13 -0500

    testtts

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+afterEach(cleanup);
+describe('About component', () => {
+    //renders About test
+  })
+
+  describe('About component', () => {
+    // First Test
+    it('renders', () => {
+      render(<About />);
+    });
+  
+    // Second Test
+  })
+
+  const { asFragment } = render(<About />);
+
+  expect(asFragment()).toMatchSnapshot();
